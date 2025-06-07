2df8df34a46bfe342be2f49e24bc3a45aa0faeb9
nagasaimaddula31 <nagasaimaddula@gmail.com>
2025-06-07 15:18:08 +0530

    NF10 fix

--- src/test/java/Utils/LoginService.java
+++ src/test/java/Utils/LoginService.java
@@ -3,8 +3,8 @@ package Utils;
 public class LoginService {
     public void login() {
     	System.out.println("User login started - Login initiated for user");
-    	System.out.println("User login started");
-    	System.out.println("User logout");
+    	
+    	System.out.println("User logoutingg");
 
     }
 }
