803b802080073ff1e9627cda2ca38e7138eb275a
nagasaimaddula31 <nagasaimaddula@gmail.com>
2025-06-07 15:05:55 +0530

    NF8 fix

--- src/test/java/Utils/LoginService.java
+++ src/test/java/Utils/LoginService.java
@@ -3,5 +3,6 @@ package Utils;
 public class LoginService {
     public void login() {
     	System.out.println("User login started - Login initiated for user");
+    	System.out.println("User login started");
     }
 }
